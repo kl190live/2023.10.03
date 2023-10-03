79cc27e96825f80031d152bbc5ecf4cff52e3fff
Petrik Diak <diak@petrik.hu>
2023-10-03 10:16:21 +0200

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023._10._03
+{
+	internal class Person
+	{
+		private string name;
+		private int age;
+		private bool gender;
+
+
+
+
+	}
+}
