b032fe68b398b7e3183c2be71345a4ac3cd53738
Petrik Diak <diak@petrik.hu>
2023-10-03 11:23:35 +0200

    Mentor class complete

--- Mentor.cs
+++ Mentor.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace _2023._10._03
 {
-	internal class Mentor
+	internal class Mentor : Person
 	{
+		private string level;
+
+		public Mentor(string level, string name, int age, string gender) : base(name,age,gender)
+		{
+			this.Level = level;
+		}
+
+		public string Level { get => level; set => level = value; }
+
+		public Mentor()
+		{
+			this.level = "intermediate";
+		}
+
+		public override string getGoal()
+		{
+			return "Educate brilliant junior software developers.";
+		}
+		public override string introduce()
+		{
+			return $"{base.introduce()} {this.level} Mentor";
+		}
+
+
 	}
 }
